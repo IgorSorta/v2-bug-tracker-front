931f6ea2b95b893dfa28fe9e4d5a01396b54998d
IgorSorta <igorsorta@gmail.com>
2020-08-10 15:23:16 +0300

    Update code in app.js file.

--- app/app.js
+++ app/app.js
@@ -2,46 +2,15 @@ const express = require('express');
 const {
     graphqlHTTP
 } = require('express-graphql');
-const {
-    buildSchema,
-} = require('graphql');
 
-const userSchema = require('./graphql/schema/user');
-const messageSchema = require('./graphql/schema/message');
-const userRes = require('./graphql/resolvers/user');
-const messageRes = require('./graphql/resolvers/message');
-
-// Test chema
-const schema = buildSchema(`
-  type Query {
-    ${userSchema.query}
-    ${messageSchema.query}
-  }
-
-  ${userSchema.type}
-  ${messageSchema.type}
-
-`);
-
-// Test resolvers
-// const resolvers = {
-//     hello: () => ({
-//         name: 'Jojo'
-//     }),
-//     twitt: () => ({
-//         text: 'Good!'
-//     }),
-// };
-const resolvers = {
-    ...userRes,
-    ...messageRes
-};
+const schema = require('./graphql/schema') // Import schema
+const resolvers = require('./graphql/resolvers'); // Import resolvers
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: true, // for test purposes
 }));
 
 // Server 
