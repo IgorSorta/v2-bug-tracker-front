9690dd6276a7f793a7e6dde5db6c29a232f5a199
IgorSorta <igorsorta@gmail.com>
2020-08-21 17:04:54 +0300

    Update resolvers files code.

--- graphql/resolvers/bug.js
+++ graphql/resolvers/bug.js
@@ -1,21 +1,31 @@
 'use strict';
-const axios = require('axios');
+const requestApi = require('../../helpers/requestApi');
+
+const requestUser = requestApi('http://127.0.0.1:3000/user');
+const requestBug = requestApi('http://127.0.0.1:3000/bug');
 
 module.exports = {
     Query: {
-        // *Get all bugs from DB
-        bugs: async () => {
+        //TODO *Get all bugs from DB
+        bugs: async (parent, args, context) => {
             try {
-
+                const bugs = requestBug();
+                return await bugs;
             } catch (error) {
                 return error;
             }
 
         },
-        // *Get bug by its id(uuidv4)
-        bug: async () => {
+        //TODO *Get bug by its id(uuidv4)
+        bug: async (parent, args, context) => {
+            const {
+                id
+            } = args;
             try {
-
+                const bug = requestBug({
+                    id: id
+                });
+                return await bug;
             } catch (error) {
                 return error;
             }
@@ -23,34 +33,70 @@ module.exports = {
 
     },
     Mutation: {
-        // *Create new bug ticket
-        createBug: async () => {
+        //TODO *Create new bug ticket
+        createBug: async (parent, args, context) => {
+            const {
+                title,
+                description
+            } = args;
+            const {
+                me
+            } = context;
             try {
-
+                const result = requestBug({
+                    title: title,
+                    description: description,
+                    userId: me.id
+                }, 'POST');
+                return await result;
             } catch (error) {
                 return error;
             }
         },
-        // *Change bug ticket status (see bug schema def.)
-        changeStatus: async () => {
+        //TODO *Change bug ticket status (see bug schema def.)
+        changeStatus: async (parent, args, context) => {
+            const {
+                id,
+                status
+            } = args;
             try {
-
+                const result = requestBug({
+                    id: id,
+                    status: status
+                }, 'POST');
+                return await result;
             } catch (error) {
                 return error;
             }
 
         },
-        // *Set bug ticket priority (see bug schema def.)
-        setPriority: async () => {
+        //TODO *Set bug ticket priority (see bug schema def.)
+        setPriority: async (parent, args, context) => {
+            const {
+                id,
+                priority
+            } = args;
             try {
-
+                const result = requestBug({
+                    id: id,
+                    priority: priority
+                }, 'PATCH');
+                return await result;
             } catch (error) {
                 return error;
             }
         },
-        // *Delete bug ticket by its id(uuidv5)
-        deleteBug: async () => {
-            try {} catch (error) {
+        //TODO *Delete bug ticket by its id(uuidv5)
+        deleteBug: async (parent, args, context) => {
+            const {
+                id
+            } = args;
+            try {
+                const result = requestBug({
+                    id: id
+                }, 'DELETE');
+                return await result;
+            } catch (error) {
                 return error;
             }
 
@@ -58,9 +104,16 @@ module.exports = {
     },
 
     Bug: {
-        // if requested bug with user data
-        user: async () => {
-
+        //TODO if requested bug with user data
+        user: async (bug, args, context) => {
+            try {
+                const user = requestUser({
+                    id: bug.userId
+                });
+                return await user;
+            } catch (error) {
+                return error;
+            }
         }
     },
 };
--- graphql/resolvers/message.js
+++ graphql/resolvers/message.js
@@ -1,38 +1,65 @@
 'use strict';
-const axios = require('axios');
+const requestApi = require('../../helpers/requestApi');
+
+const requestUser = requestApi('http://127.0.0.1:3000/user');
+const requestMessage = requestApi('http://127.0.0.1:3000/message');
 
 module.exports = {
-    // *Get all messages in DB
+    //TODO *Get all messages in DB
     Query: {
-        messages: async () => {
+        messages: async (parent, args, context) => {
             try {
-
+                const messages = requestMessage();
+                return await messages;
             } catch (error) {
                 return error;
             }
 
         },
-        // *Get message by its id(uuidv4)
-        message: async () => {
+        //TODO *Get message by its id(uuidv4)
+        message: async (parent, args, context) => {
+            const {
+                id
+            } = args;
             try {
-
+                const message = requestMessage({
+                    id: id
+                })
+                return await message;
             } catch (error) {
                 return error;
             }
         },
     },
     Mutation: {
-        // *Create new message
-        createMessage: async () => {
+        //TODO *Create new message
+        createMessage: async (parent, args, context) => {
+            const {
+                text
+            } = args;
+            const {
+                me
+            } = context;
             try {
-
+                const result = requestMessage({
+                    text: text,
+                    userId: me.id
+                }, 'POST');
+                return await result;
             } catch (error) {
                 return error;
             }
         },
-        deleteMessage: async () => {
+        // TODO Delete message
+        deleteMessage: async (parent, args, context) => {
+            const {
+                id
+            } = args;
             try {
-
+                const result = requestMessage({
+                    id: id
+                }, 'DELETE');
+                return await result;
             } catch (error) {
                 return error;
             }
@@ -40,10 +67,13 @@ module.exports = {
         },
     },
     Message: {
-        // if request message with user data
-        user: async () => {
+        //TODO if request message with user data
+        user: async (message, args, context) => {
             try {
-
+                const user = requestUser({
+                    id: message.userId
+                });
+                return await user;
             } catch (error) {
                 return error;
             }
